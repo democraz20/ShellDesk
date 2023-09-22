e330684427cce42349d75c2b7d62bb71a45901f5
democraz20 <mozartrasig@gmail.com>
2023-09-23 02:07:05 +0700

    current working vers

--- src/main.rs
+++ src/main.rs
@@ -9,70 +9,92 @@ use crossterm::{
         enable_raw_mode
     }
 };
-use std::{io::{self, Write}, error::Error, fmt::format, hash::Hash, iter::Cloned, path::Path};
-use std::collections::HashSet;
+use std::{
+    io::{self, Write}, 
+    error::Error, 
+    fmt::format, 
+    hash::Hash, 
+    iter::Cloned, 
+    path::Path,
+    fs
+};
+
+use std::ptr::null_mut;
+use winapi::um::winuser::{
+    GetForegroundWindow, ShowWindow, SW_MAXIMIZE, SWP_FRAMECHANGED, GWL_STYLE, SW_HIDE, SW_RESTORE,
+    GetWindowLongA, SetWindowLongA, WS_POPUP, WS_CAPTION, WS_SYSMENU, WS_THICKFRAME, WS_MINIMIZEBOX, WS_MAXIMIZEBOX,
+};
+
 use std::io::stdout;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
+
+    //window prep
+    unsafe {
+        // Get the handle of the currently focused window
+        let hwnd = GetForegroundWindow();
+
+        if hwnd.is_null() {
+            panic!("Failed to get the foreground window handle");
+        }
+
+        // Check if the window is maximized
+        let is_maximized = GetWindowLongA(hwnd, GWL_STYLE) & WS_MAXIMIZEBOX as i32 != 0;
+
+        // If the window is maximized, restore it to normal state
+        if is_maximized {
+            ShowWindow(hwnd, SW_RESTORE);
+        }
+
+        // Remove the WS_CAPTION style to hide the window title bar
+        let mut window_style = GetWindowLongA(hwnd, GWL_STYLE) as u32;
+        window_style &= !(WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
+        SetWindowLongA(hwnd, GWL_STYLE, window_style as i32);
+
+        // Maximize the window
+        ShowWindow(hwnd, SW_MAXIMIZE);
+        ShowWindow(hwnd, SWP_FRAMECHANGED.try_into().unwrap());
+
+        println!("Window maximized to full-screen.");
+    }
+
+
     execute!(stdout(), EnterAlternateScreen)?;
     enable_raw_mode()?;
-    
-    // Create a boolean variable with an Arc (atomic reference counter) and Mutex for thread safety.
-    let flag = Arc::new(Mutex::new(false));
 
-    // Clone the Arc for the thread.
-    let flag_clone = Arc::clone(&flag);
-    // Spawn a new thread to modify the variable to true after a delay.
-    println!("start thread");
-    let handle = thread::spawn(move || {
-        // Modify the variable to true.
-
-        loop {
-            match event::read().unwrap() {
-                Key(KeyEvent {
-                    code: KeyCode::Char('q'),
-                    modifiers: KeyModifiers::CONTROL,
-                    kind: KeyEventKind::Press,
-                    state: _,
-                }) => {
-                    // Exit the loop on Ctrl + Q.
-                    // break;
-                    println!("exiting 1");
-                    let mut data = flag_clone.lock().unwrap();
-                    println!("test1");
-                    *data = true;
-                    println!("test2");
-                    break;
-                },
-                Key(KeyEvent{
-                    code: KeyCode::Char(c),
-                    kind: KeyEventKind::Press, ..
-                }) => {
-                    println!("pressed : {}", c)
-                }
-                Key(KeyEvent{
-                    code:KeyCode::F(5),
-                    kind: KeyEventKind::Press, ..
-                }) => {
-                }
-                _ => {}
-                
-            }
-        }
-        println!("end of created thread");
-    });
-    println!("started thread");
-    // Main thread continuously checks the variable until it becomes true.
-    loop {
-        // Lock the mutex to access the variable.
-        thread::sleep(std::time::Duration::from_secs(1));
-        let data = flag.lock().unwrap();
-        if *data {
-            println!("Flag is true. Breaking the loop.");
-            break;
-        }
-        drop(data); // Release the lock before sleeping to allow other threads to access it.
+    let paths = fs::read_dir("./").unwrap();
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
     }
+
+
+    loop {
+        match event::read().unwrap() {
+            Key(KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::CONTROL,
+                kind: KeyEventKind::Press,
+                state: _,
+            }) => {
+                // Exit the loop on Ctrl + Q.
+                // break;
+                break;
+            },
+            Key(KeyEvent{
+                code:KeyCode::F(5),
+                kind: KeyEventKind::Press, ..
+            }) => {
+
+            }
+            _ => {}
+            
+        }
+    }
+
+    let handle = thread::spawn(move || {
+        //handle file changes here
+
+    });
     
     execute!(stdout(), LeaveAlternateScreen)?;
     disable_raw_mode()?;
@@ -81,25 +103,3 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     Ok(())
 }
 
-
-
-// fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
-//     let (tx, rx) = std::sync::mpsc::channel();
-
-//     // Automatically select the best implementation for your platform.
-//     // You can also access each implementation directly e.g. INotifyWatcher.
-//     let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
-
-//     // Add a path to be watched. All files and directories at that path and
-//     // below will be monitored for changes.
-//     watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
-
-//     for res in rx {
-//         match res {
-//             Ok(event) => println!("Change: {event:?}"),
-//             Err(error) => println!("Error: {error:?}"),
-//         }
-//     }
-
-//     Ok(())
-// }
