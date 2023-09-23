6ee1b8a0ffb97556a62bd1f64643fbcf05d9da13
democraz20 <mozartrasig@gmail.com>
2023-09-23 23:19:13 +0700

    borders and Ndisplayable

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,19 @@ use crossterm::{
 };
 use std::{
     io::{self, Write}, 
-    error::Error, 
-    fmt::format, 
-    hash::Hash, 
-    iter::Cloned, 
-    path::Path,
     fs
 };
 
-use std::ptr::null_mut;
-use winapi::um::winuser::{
-    GetForegroundWindow, ShowWindow, SW_MAXIMIZE, SWP_FRAMECHANGED, GWL_STYLE, SW_RESTORE,
-    GetWindowLongA, SetWindowLongA, WS_CAPTION, WS_SYSMENU, WS_THICKFRAME, WS_MINIMIZEBOX, WS_MAXIMIZEBOX,
-};
 
 use std::io::stdout;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
-
+    
+    // use std::ptr::null_mut;
+    // use winapi::um::winuser::{
+    //     GetForegroundWindow, ShowWindow, SW_MAXIMIZE, SWP_FRAMECHANGED, GWL_STYLE, SW_RESTORE,
+    //     GetWindowLongA, SetWindowLongA, WS_CAPTION, WS_SYSMENU, WS_THICKFRAME, WS_MINIMIZEBOX, WS_MAXIMIZEBOX,
+    // };
     //window prep
     // unsafe {
     //     // Get the handle of the currently focused window
@@ -69,22 +64,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     enable_raw_mode()?;
     execute!(stdout(), MoveTo(1,1))?; //1 cell margin
 
-    let paths = fs::read_dir("./")?;
-    let paths = custom_sort(paths);
-    println!("=PATHS=");
+    
+    let dirItems = fs::read_dir("./")?;
+    let dirItems = custom_sort(dirItems);
+    println!("=LIST=");
     let (cursorX, cursorY) = cursor::position()?;
     let (columns, rows) = terminal::size()?;
+    
+    print_borders(columns, rows)?;
+    
+    let longest_item: String;
+    match dirItems.iter().max_by_key(|s| s.len()) {
+        Some(s) => {
+            longest_item = String::from(s);
+        },
+        None => {
+            longest_item = String::from("Directory is empty");
+        }
+    }
+    let Ndisplayable = ((columns-2) as f32 / (longest_item.len()+2) as f32).floor();
+    // let Ndisplayable = div.floor();
 
+    
+    execute!(stdout(), MoveTo(1,0))?;
+    print!("termsize : {}x{}, longest item: {}, n displayable: {}", columns, rows, longest_item.len(), Ndisplayable);
+    stdout().flush()?;
+    
     execute!(stdout(), MoveTo(cursorX+2,cursorY))?;
-
-    for path in paths.clone() {
+    for path in dirItems.clone() {
         // println!("Name: {}", path);
         // print!("");
         stdout().flush()?;
         if PathBuf::from(&path).is_dir() {
-            print!("{}  ", path.blue().bold());
+            print!("{}{}", path.clone().blue().bold(), " ".repeat(longest_item.len()-path.len()+2));
         } else {
-            print!("{}  ", path);
+            print!("{}{}", path, " ".repeat(longest_item.len()-path.len()+2));
         }
     }
 
@@ -108,7 +122,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                 code: KeyCode::Right,
                 kind: KeyEventKind::Press, ..
             }) => {
-                if !(selected > paths.len()-1) {
+                if !(selected > dirItems.len()-1) {
                     selected += 1;
                     execute!(stdout(), MoveTo(1, rows-2))?;
                     print!("adding 1, is now {}", selected);
@@ -183,4 +197,48 @@ fn custom_sort(read_dir: ReadDir) -> Vec<String> {
         .iter()
         .map(|entry| entry.file_name().to_string_lossy().to_string())
         .collect()
+}
+
+fn print_borders(X: u16, Y: u16) -> Result<(), Box<dyn std::error::Error>> {
+    let top_left = '\u{02554}';
+    let top_right = '\u{02557}';
+    let bottom_left = '\u{0255A}';
+    let bottom_right = '\u{0255D}';
+
+    let vertical = '\u{02551}';
+    let horizontal = '\u{02550}';
+
+    //corners
+
+    execute!(stdout(), MoveTo(0,0))?;
+    print!("{}", top_left); //╔
+    
+    execute!(stdout(), MoveTo(X, 0))?;
+    print!("{}", top_right); 
+
+    execute!(stdout(), MoveTo(0, Y))?;
+    print!("{}", bottom_left); 
+
+    execute!(stdout(), MoveTo(X, Y))?;
+    print!("{}", bottom_right); 
+
+    //sides
+
+    execute!(stdout(), MoveTo(1,0))?;
+    print!("{}", String::from(horizontal).repeat((X-2).into()));
+
+    execute!(stdout(), MoveTo(1,Y))?;
+    print!("{}", String::from(horizontal).repeat((X-2).into()));
+
+    for i in 0..Y-2 {
+        execute!(stdout(), MoveTo(0,i+1))?;
+        print!("{}", vertical);
+    }
+
+    for i in 0..Y-2 {
+        execute!(stdout(), MoveTo(X,i+1))?;
+        print!("{}", vertical);
+    }
+
+    Ok(())
 }
