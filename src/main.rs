941552e326abc0e557dbbac5e4919e96f8d379b0
democraz20 <mozartrasig@gmail.com>
2024-03-09 00:39:35 +0700

    test commit 2

--- src/main.rs
+++ src/main.rs
@@ -70,8 +70,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // print_borders(columns, rows)?;
     // execute!(stdout(), MoveTo(0,1))?;
 
-    // println!("col : rows | {} : {}", columns, rows);
-
     match UdirItems.iter().max_by_key(|s| s.len()) {
         Some(s) => {
             longest_item = String::from(s);
